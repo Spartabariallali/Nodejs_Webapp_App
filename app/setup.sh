0f33933e3e0155c5ee9d92f8c884a31c11f8251e
MOH <iAllali@Baris-macbook-pro.local>
2020-08-07 19:25:32 +0100

    removed uncessary provisions.sh in environment directory

--- app/setup.sh
+++ app/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # setting up
-scp -i ~/.ssh/DevOpsStudents.pem -r ~/Desktop/nginx_sample_code/nodejs-aws-deploy/app ubuntu@ec2-52.211.100.82.eu-west-1.compute.amazonaws.com:/home/ubuntu/
+# scp -i ~/.ssh/DevOpsStudents.pem -r ~/Desktop/nginx_sample_code/nodejs-aws-deploy/app ubuntu@ec2-52.211.100.82.eu-west-1.compute.amazonaws.com:/home/ubuntu/
+scp -i ~/.ssh/DevOpsStudents.pem -r ~/Desktop/nginx_sample_code/nodejs-aws-deploy/app ubuntu@ec2-52-211-100-82.eu-west-1.compute.amazonaws.com:/home/ubuntu/
 
 
 ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@ec2-52.211.100.82.eu-west-1.compute.amazonaws.com -i $ mkdir environment
