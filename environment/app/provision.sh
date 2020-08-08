6cd49d6b40794b2800bd492f542d86b2faf77cfe
MOH <iAllali@Baris-macbook-pro.local>
2020-08-08 16:49:12 +0100

    created seperate provisions for app and db ami

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -16,8 +16,10 @@ sudo touch reverse-proxy.conf
 sudo chmod 666 reverse-proxy.conf
 echo "server{
   listen 80;
+  server_name development.local;
+
   location / {
-      proxy_pass http://192.168.10.100:3000;
+      proxy_pass http://127.0.0.1:3000;
   }
 }" >> reverse-proxy.conf
 sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
@@ -33,21 +35,3 @@ sudo apt-get install nodejs -y
 
 # install pm2
 sudo npm install pm2 -g
-
-# App set up
-export DB_HOST="mongodb://192.168.10.150:27017/posts"
-cd /home/ubuntu/app
-sudo su
-npm install
-# node app.js
-
-scp  -i ~/.ssh/DevOpsStudents.pem -r app/ ubuntu@ec2-34-248-99-137.eu-west-1.compute.amazonaws.com:~/app/
-
-# scp -i ~/.ssh/DevOpsStudents.pem -r app/ ubuntu@ 34.248.99.137:~/app/
-
-# # remove the old file and add our one
-# sudo rm /etc/nginx/sites-available/default
-# sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default
-#
-# # finally, restart the nginx service so the new config takes hold
-# sudo service nginx restart
